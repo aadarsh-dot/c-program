234be2f7e6b0e2d2e7f7a59306b4bdf05b30cb26
aadarsh-dot <imca2401@rajagiri.edu>
2024-10-19 09:39:19 +0530

    Add files via upload

---
+++ Untitled10.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float r;
+	printf("enter radius");
+	scanf("%f",&r);
+	printf("area of circle is %.1f",3.14*r*r);
+	return 0;	
+}
---
+++ Untitled9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter first number");
+	scanf("%d",&a);
+	printf("enter second number");
+	scanf("%d",&b);
+	printf("a=%d b=%d",b,a);
+	return 0;	
+}
