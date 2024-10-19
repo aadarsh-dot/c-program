5b6f5d3a5767e1f5f22921575067d4313e851bee
aadarsh-dot <imca2401@rajagiri.edu>
2024-10-19 09:51:12 +0530

    Add files via upload

---
+++ convert kilometers to miles.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float k;
+	printf("enter kilometers");
+	scanf("%f",&k);
+	printf("conversion of the entered kilometers to miles is %.1f",k*0.621371);
+	return 0;
+}
